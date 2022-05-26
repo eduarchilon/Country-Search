9110a10bb53100d2ac67a8cafa3b5b884a637535
jazmindestefano <jazminisabeldestefano@gmail.com>
2022-05-26 14:42:20 -0300

    add createflag

---
+++ js/app/Flag.js
@@ -0,0 +1,22 @@
+export function createFlagCountry(country) {
+    let flag = ''
+    flag += `<div class="country__content">
+            <img class="country__image" src="${country.flags.png}"></img>
+            <div class="country__information">
+                <h3 class="country_name">${country.name.common}</h3>
+                <p class= "country__population"> Population: ${pais.population}</p>
+                <p class="country__region"> Region: ${country.region}</p>
+                <p class="country__capital"> Capital: ${country.capital}</p>
+                </div>
+            </div>
+        </div>`
+    return (document.getElementById('country__container').innerHTML = flag);
+  }
+  
+  export function createFlagContinent(continent) {
+    let flags = ''
+    for (let i = 0; i < continent.length; i++) {
+      flags += createFlag(continent[i])
+    }
+    document.getElementById('country__container').innerHTML = flags;
+  }
