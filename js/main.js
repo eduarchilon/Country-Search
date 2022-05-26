5c8f8d6011cf4b7d0e8aeacc12172efb89321653
jazmindestefano <jazminisabeldestefano@gmail.com>
2022-05-26 17:13:20 -0300

    add getCountriesByResearch function

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
-import "../js/app/busqueda.js"
 import { result } from './fetch.js'
 
+const input = document.getElementById('#searchInput');
 
 const createCountryCard = ({ flags, name, population, region, capital }) => {
     let countries = document.getElementById('country__container');
@@ -18,12 +18,29 @@ const createCountryCard = ({ flags, name, population, region, capital }) => {
 }
 
 const getCountries = async (api) =>{
-
     const arrayCountries = await api.json();
     arrayCountries.forEach(element => {
         createCountryCard(element);
     });
 }
 
-getCountries(result);
 
+const getCountriesByResearch = async (event) => {
+    const value = event.target.value;
+    const result = await fetch(`https://restcountries.com/v3.1/name/${value}`);
+    const results = await result.json();
+    if(input.value === ""){
+        getCountries();
+    } else {
+        if(result.status === 200){
+            getCountries(results);
+        } else if (result.status === 404) {
+            alert("We couldnt find this country");
+        }
+    }
+    return results;
+}
+
+input.addEventListener('input', getCountriesByResearch);
+
+getCountries(result);
