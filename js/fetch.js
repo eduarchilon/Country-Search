94b35579dcaf5f755452ec74cb34490d145d306c
Eduar <83920925+eduarchilon@users.noreply.github.com>
2022-05-27 09:31:47 -0300

    feat/ APIv2 to APIv3

--- js/fetch.js
+++ js/fetch.js
@@ -1,2 +1,2 @@
 /* Aca nos trae el objeto de la api */
-export const result = await fetch('https://restcountries.com/v2/all');
+export const result = await fetch('https://restcountries.com/v3.1/all');
--- js/main.js
+++ js/main.js
@@ -8,9 +8,9 @@ export const arrayCountries = await result.json();
 export const createCountryCard = ({ flags, name, population, region, capital }) => {
     const countries = `
                     <div class="country__content" id="country__content">
-                        <img class="country__image" src="${flags.png}" alt="${name}" width="250">
+                        <img class="country__image" src="${flags.png}" alt="${name.common}" width="250">
                         <div class="country__information">
-                            <h5 class="country__name text"> ${name}</h5>
+                            <h5 class="country__name text"> ${name.common}</h5>
                             <p class="country__population text">Population: <span class="text text-info"> ${population}</span></p>
                             <p class="country__region text">Region: <span class="text text-info"> ${region}</span></p>
                             <p class="country__capital text">Capital: <span class="text text-info"> ${capital}</span></p>
